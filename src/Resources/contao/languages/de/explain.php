097d54d5ad6baecce5bae32ff0adf984ad9e6f4a
Glen Langer <contao.modul@glen-langer.de>
2019-05-11 23:35:56 +0200

    Update language files (de)

--- src/Resources/contao/languages/de/explain.php
+++ src/Resources/contao/languages/de/explain.php
@@ -1,15 +1,29 @@
 <?php
-$GLOBALS['TL_LANG']['XPL']['calculate_help']['0']['0'] = 'Fictional Stardate nach TrekGuide.com (Today)';
-$GLOBALS['TL_LANG']['XPL']['calculate_help']['0']['1'] = 'Fictional Stardate nach TrekGuide.com, positives Ergebnis für unsere Zeit.<br>YYMM.DD : YY = YYYY-1900';
-$GLOBALS['TL_LANG']['XPL']['calculate_help']['2']['0'] = 'Fictional Stardate nach TrekGuide.com (1946)';
-$GLOBALS['TL_LANG']['XPL']['calculate_help']['2']['1'] = 'Fictional Stardate nach TrekGuide.com, positives Ergebnis für unsere Zeit.<br>Sternzeit 00000.0 beginnt am 14.07.1946, um 18:00 Uhr.';
-$GLOBALS['TL_LANG']['XPL']['calculate_help']['3']['0'] = 'Fictional Stardate nach TrekConnection.com';
-$GLOBALS['TL_LANG']['XPL']['calculate_help']['3']['1'] = 'Stardate Variante nach TrekConnection.com, positives Ergebnis für unsere Zeit.';
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/contao-stardate-bundle/language/de/
+ *
+ * last-updated: 2019-05-05T16:54:35+02:00
+ */
+
+
+$GLOBALS['TL_LANG']['XPL']['calculate_help']['0']['0'] = 'Fiktive Sternzeit nach TrekGuide.com (Today)';
+$GLOBALS['TL_LANG']['XPL']['calculate_help']['0']['1'] = 'Fiktive Sternzeit nach TrekGuide.com, positives Ergebnis für unsere Zeit.<br>YYMM.DD : YY = YYYY-1900';
+$GLOBALS['TL_LANG']['XPL']['calculate_help']['2']['0'] = 'Fiktive Sternzeit nach TrekGuide.com (1946)';
+$GLOBALS['TL_LANG']['XPL']['calculate_help']['2']['1'] = 'Fiktive Sternzeit nach TrekGuide.com, positives Ergebnis für unsere Zeit.<br>Sternzeit 00000.0 beginnt am 14.07.1946, um 18:00 Uhr.';
+$GLOBALS['TL_LANG']['XPL']['calculate_help']['3']['0'] = 'Fiktive Sternzeit nach TrekConnection.com';
+$GLOBALS['TL_LANG']['XPL']['calculate_help']['3']['1'] = 'Fiktive Sternzeit nach TrekConnection.com, positives Ergebnis für unsere Zeit.';
 $GLOBALS['TL_LANG']['XPL']['calculate_help']['4']['0'] = 'ST XI Variante nach TrekGuide.com';
 $GLOBALS['TL_LANG']['XPL']['calculate_help']['4']['1'] = 'ST XI Variante nach TrekGuide.com, positives Ergebnis für unsere Zeit.<br>YYYY.xx : YYYY aktuelles Jahr, xx 100. Teil des Jahres';
-$GLOBALS['TL_LANG']['XPL']['calculate_help']['5']['0'] = 'Star Trek TNG Stardate (2323)';
+$GLOBALS['TL_LANG']['XPL']['calculate_help']['5']['0'] = 'Star Trek TNG Sternzeit (2323)';
 $GLOBALS['TL_LANG']['XPL']['calculate_help']['5']['1'] = 'TNG Variante 1 nach lcars.org.uk, negatives Ergebnis für unsere Zeit.<br>Sternzeit 00000.0 beginnt am 01.01.2323, um 00:00 Uhr.';
-$GLOBALS['TL_LANG']['XPL']['calculate_help']['6']['0'] = 'Star Trek TNG Stardate (2322)';
+$GLOBALS['TL_LANG']['XPL']['calculate_help']['6']['0'] = 'Star Trek TNG Sternzeit (2322)';
 $GLOBALS['TL_LANG']['XPL']['calculate_help']['6']['1'] = 'TNG Variante 2 nach TrekGuide.com, negatives Ergebnis für unsere Zeit.<br>Sternzeit 00000.0 beginnt am 25.05.2322, um 00:00 Uhr.';
-$GLOBALS['TL_LANG']['XPL']['calculate_help']['7']['0'] = 'Star Trek TOS Stardate (2265)';
+$GLOBALS['TL_LANG']['XPL']['calculate_help']['7']['0'] = 'Star Trek TOS Sternzeit (2265)';
 $GLOBALS['TL_LANG']['XPL']['calculate_help']['7']['1'] = 'TOS Variante nach TrekGuide.com, negatives Ergebnis für unsere Zeit.<br>Sternzeit 00000.0 beginnt am 01.05.2265, um 00:00 Uhr.';
+
--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -4,20 +4,16 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_content']['calculate']['0'] = 'Berechnung';
-$GLOBALS['TL_LANG']['tl_content']['calculate']['1'] = 'Art der Berechnung auswählen';
-/**
- * Legends
- */
+
+$GLOBALS['TL_LANG']['tl_content']['calculate']['0']   = 'Berechnung';
+$GLOBALS['TL_LANG']['tl_content']['calculate']['1']   = 'Art der Berechnung auswählen';
 $GLOBALS['TL_LANG']['tl_content']['calculate_legend'] = 'Berechnungsarten';
-/**
- * Options
- */
 $GLOBALS['TL_LANG']['tl_content']['default']          = 'Bitte auswählen';
-$GLOBALS['TL_LANG']['tl_content']['trekguide_f1']     = 'Fictional Stardate nach TrekGuide.com (Today)';
-$GLOBALS['TL_LANG']['tl_content']['trekguide_f2']     = 'Fictional Stardate nach TrekGuide.com (1946)';
-$GLOBALS['TL_LANG']['tl_content']['trekconnection']   = 'Fictional Stardate nach TrekConnection.com';
+$GLOBALS['TL_LANG']['tl_content']['startrek_tng2322'] = 'Star Trek TNG Sternzeit nach TrekGuide.com (2322)';
+$GLOBALS['TL_LANG']['tl_content']['startrek_tng2323'] = 'Star Trek TNG Sternzeit nach lcars.org.uk (2323)';
+$GLOBALS['TL_LANG']['tl_content']['startrek_tos2265'] = 'Star Trek TOS Sternzeit nach TrekGuide.com (2265)';
+$GLOBALS['TL_LANG']['tl_content']['trekconnection']   = 'Fiktive Sternzeit nach TrekConnection.com';
+$GLOBALS['TL_LANG']['tl_content']['trekguide_f1']     = 'Fiktive Sternzeit nach TrekGuide.com (Today)';
+$GLOBALS['TL_LANG']['tl_content']['trekguide_f2']     = 'Fiktive Sternzeit nach TrekGuide.com (1946)';
 $GLOBALS['TL_LANG']['tl_content']['trekguide_x11']    = 'ST XI Variante nach TrekGuide.com';
-$GLOBALS['TL_LANG']['tl_content']['startrek_tng2323'] = 'Star Trek TNG Stardate nach lcars.org.uk (2323)';
-$GLOBALS['TL_LANG']['tl_content']['startrek_tng2322'] = 'Star Trek TNG Stardate nach TrekGuide.com (2322)';
-$GLOBALS['TL_LANG']['tl_content']['startrek_tos2265'] = 'Star Trek TOS Stardate nach TrekGuide.com (2265)';
+
