9b936330423438b369478bed9ad520ee33f36e64
Glen Langer <contao.modul@glen-langer.de>
2019-07-25 01:38:44 +0200

    Run php-cs-fixer

--- src/Runonce/CompileCommands.php
+++ src/Runonce/CompileCommands.php
@@ -14,37 +14,41 @@ declare(strict_types=1);
 
 namespace BugBuster\StardateBundle\Runonce;
 
+/**
+ * Class CompileCommands.
+ *
+ * Call over the hook sqlCompileCommands
+ */
 class CompileCommands
 {
     /**
-     * Hook Call
+     * Hook Call sqlCompileCommands.
      *
-     * @param  array $definition Array of SQL statements
-     * @return array             Array of SQL statements
+     * @param array $definition Array of SQL statements
+     *
+     * @return array Array of SQL statements
      */
     public function runMigration(array $definition)
     {
         $this->runMigration200($definition);
-        log_message(sprintf('[%s] %s','GetFromDbHook::runMigration',print_r($definition,true)),'stardate_debug.log');
-        $definition = $this->manipulateSqlCommands($definition);
-        log_message(sprintf('[%s] %s','GetFromDbHook::manipulatSqlCommands',print_r($definition,true)),'stardate_debug.log');
-        return $definition;
+
+        return $this->manipulateSqlCommands($definition);
     }
 
     /**
-     * Delete the ALTER TABLE command
-     * 
-     * @param  array $return Array of SQL statements
-     * @return array         Array of SQL statements
+     * Delete the ALTER TABLE command.
+     *
+     * @param array $return Array of SQL statements
+     *
+     * @return array Array of SQL statements
      */
     public function manipulateSqlCommands($return)
     {
-        if (is_array($return['ALTER_CHANGE'])) 
-        {
+        if (\is_array($return['ALTER_CHANGE'])) {
             $return['ALTER_CHANGE'] = array_filter(
                 $return['ALTER_CHANGE'],
                 function ($sql) {
-                    return strpos($sql, 'ALTER TABLE tl_content CHANGE calculate calculateStardate') === false;
+                    return false === strpos($sql, 'ALTER TABLE tl_content CHANGE calculate calculateStardate');
                 }
             );
 
@@ -52,37 +56,35 @@ class CompileCommands
                 unset($return['ALTER_CHANGE']);
             }
         }
+
         return $return;
     }
 
     /**
-     * Run the migration to version 2.0.0
+     * Run the migration to version 2.0.0.
      *
      * @param array $definition Array of SQL statements
-     * @return void
      */
-    public function runMigration200(array $definition)
+    public function runMigration200(array $definition): void
     {
         $migration = false;
-        if (\Contao\Database::getInstance()->tableExists('tl_content'))
-        {
+        if (\Contao\Database::getInstance()->tableExists('tl_content')) {
             if (\Contao\Database::getInstance()->fieldExists('calculate', 'tl_content')
-            && !\Contao\Database::getInstance()->fieldExists('calculateStardate', 'tl_content'))
-            {
-                //\Contao\Database::getInstance()->execute("ALTER TABLE `tl_content` ADD `calculateStardate` varchar(255) NOT NULL default ''");
+            && !\Contao\Database::getInstance()->fieldExists('calculateStardate', 'tl_content')) {
                 \Contao\Database::getInstance()->execute("ALTER TABLE tl_content CHANGE calculate calculatestardate VARCHAR(255) DEFAULT '' NOT NULL");
                 \Contao\Database::getInstance()->prepare("UPDATE `tl_content` SET `type`='stardate' WHERE `type`=?")
-                                                ->execute('content_stardate');
+                                                ->execute('content_stardate')
+                ;
                 $migration = true;
             }
         }
 
-        if ($migration === true)
-        {
+        if (true === $migration) {
             //Protokoll
             $strText = 'Stardate-Bundle has been migrated';
-            \Contao\Database::getInstance()->prepare("INSERT INTO `tl_log` (tstamp, source, action, username, text, func, browser) VALUES(?, ?, ?, ?, ?, ?, ?)")
-                            ->execute(time(), 'BE', 'CONFIGURATION', '', specialchars($strText), 'Stardate Bundle Migration', '127.0.0.1', 'NoBrowser');
+            \Contao\Database::getInstance()->prepare('INSERT INTO `tl_log` (tstamp, source, action, username, text, func, browser) VALUES(?, ?, ?, ?, ?, ?, ?)')
+                            ->execute(time(), 'BE', 'CONFIGURATION', '', specialchars($strText), 'Stardate Bundle Migration', '127.0.0.1', 'NoBrowser')
+            ;
         }
     }
 }
