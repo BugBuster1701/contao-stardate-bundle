55b983845eb01c8f1fed51e864107e2c0d283593
Glen Langer <contao.modul@glen-langer.de>
2023-09-03 02:31:20 +0200

    #14, StardateController setzt fragment Tags

--- src/Controller/ContentElement/StardateController.php
+++ src/Controller/ContentElement/StardateController.php
@@ -6,6 +6,7 @@ namespace BugBuster\StardateBundle\Controller\ContentElement;
 
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
 use Contao\ContentModel;
+use Contao\System;
 use Contao\CoreBundle\Twig\FragmentTemplate;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +15,42 @@ class StardateController extends AbstractContentElementController
 {
     protected function getResponse(FragmentTemplate $template, ContentModel $model, Request $request): Response
     {
-        $template->set('stardateTag', '0815');
+        if (System::getContainer()->get('contao.routing.scope_matcher')
+	        ->isBackendRequest(System::getContainer()->get('request_stack')
+        	->getCurrentRequest() ?? Request::create(''))) 
+        {
+            $calc = $GLOBALS['TL_LANG']['tl_content'][$model->calculateStardate];
+            $template->set('stardateTag', '=/\= '.$calc.' =/\=');
+
+            return $template->getResponse();
+        }
+
+        switch ($model->calculateStardate) {
+            case 'trekguide_f1':
+                $template->set('stardateTag', '{{fragment::{{stardate::trekguide_f1}}}}');
+                break;
+            case 'trekguide_f2':
+                $template->set('stardateTag', '{{fragment::{{stardate::trekguide_f2}}}}');
+                break;
+            case 'trekconnection':
+                $template->set('stardateTag', '{{fragment::{{stardate::trekconnection}}}}');
+                break;
+            case 'trekguide_x11':
+                $template->set('stardateTag', '{{fragment::{{stardate::trekguide_x11}}}}');
+                break;
+            case 'startrek_tng2323':
+                $template->set('stardateTag', '{{fragment::{{stardate::startrek_tng2323}}}}');
+                break;
+            case 'startrek_tng2322':
+                $template->set('stardateTag', '{{fragment::{{stardate::startrek_tng2322}}}}');
+                break;
+            case 'startrek_tos2265':
+                $template->set('stardateTag', '{{fragment::{{stardate::startrek_tos2265}}}}');
+                break;
+            default:
+                $template->set('stardateTag', '00000.00');
+                break;
+        }
 
         return $template->getResponse();
     }
