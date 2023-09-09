1d7101af0b07d9c81b3a2d4f1c15f4574fcfc7a0
Glen Langer <contao.modul@glen-langer.de>
2023-09-09 16:10:09 +0200

    #18, fix link

--- ecs.php
+++ ecs.php
@@ -11,7 +11,7 @@ return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([__DIR__.'/vendor/contao/easy-coding-standard/config/contao.php']);
 
     $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, [
-        'header' => "This file is part of a BugBuster Contao Bundle.\n\n@copyright  Glen Langer ".date('Y')." <http://contao.ninja>\n@author     Glen Langer (BugBuster)\n@package    Contao Stardate Bundle\n@license LGPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/bugbuster/contao-stardate-bundle",
+        'header' => "This file is part of a BugBuster Contao Bundle.\n\n@copyright  Glen Langer ".date('Y')." <http://contao.ninja>\n@author     Glen Langer (BugBuster)\n@package    Contao Stardate Bundle\n@license LGPL-3.0-or-later\nFor the full copyright and license information,\nplease view the LICENSE file that was distributed with this source code.\n@link https://github.com/BugBuster1701/contao-stardate-bundle",
     ]);
 
     $ecsConfig->skip([
